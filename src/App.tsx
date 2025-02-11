dbf7850d916c66a10a5163a8841645c607b19317
Dev Aggarwal <devaggarwal1009@gmail.com>
2025-02-11 22:22:43 +0530

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './Page/Home'
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> Hello World </h1>
+      {/* <NavBar/> */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import WalletProvider from './walletProvider.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WalletProvider>
-      <App />
-    </WalletProvider>
+    <BrowserRouter>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
