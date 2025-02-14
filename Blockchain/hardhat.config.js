118e0ee169c75aca19b3c1bc27c16b86b70779f4
Dev Aggarwal <devaggarwal1009@gmail.com>
2025-02-14 10:33:16 +0530

    prediton market blockchain addition

---
+++ Blockchain/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    seiTestnet: {
+      url: "https://evm-rpc-testnet.sei-apis.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
